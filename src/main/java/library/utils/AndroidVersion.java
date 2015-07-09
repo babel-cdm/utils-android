4f0000053f56fc7ed6b0c3fc7c7474d4bd1e5fc3
Ismael González Sierra <ismael.gonzalez@babel.es>
2015-07-09 13:30:20 +0200

    Añadidas utilidades de pantalla

---
+++ src/main/java/library/utils/AndroidVersion.java
@@ -0,0 +1,54 @@
+package library.utils;
+
+import android.os.Build;
+
+public class AndroidVersion {
+    private static final int VERSION_CODES_FROYO = 8;
+    private static final int VERSION_CODES_GINGERBREAD = 9;
+    private static final int VERSION_CODES_GINGERBREADM_MR1 = 10;
+    private static final int VERSION_CODES_HONEYCOMB = 11;
+    private static final int VERSION_CODES_HONEYCOMB_MR1 = 12;
+    private static final int VERSION_CODES_HONEYCOMB_MR2 = 13;
+    private static final int VERSION_CODES_JELLY_BEAN = 16;
+    private static final int VERSION_CODES_JELLY_BEAN_MR1 = 17;
+
+    public static boolean isFroyoOrUp () {
+        return (Build.VERSION.SDK_INT >= VERSION_CODES_FROYO);
+    }
+
+    public static boolean isGingerbreadOrUp () {
+        return (Build.VERSION.SDK_INT >= VERSION_CODES_GINGERBREAD);
+    }
+
+    public static boolean isGingerbreadOrDown () {
+        return (Build.VERSION.SDK_INT <= VERSION_CODES_GINGERBREAD);
+    }
+
+    public static boolean isGingerbreadMR1OrUp () {
+        return (Build.VERSION.SDK_INT >= VERSION_CODES_GINGERBREADM_MR1);
+    }
+
+    public static boolean isGingerbreadMR1OrDown () {
+        return (Build.VERSION.SDK_INT <= VERSION_CODES_GINGERBREADM_MR1);
+    }
+
+    public static boolean isHoneycombOrUp () {
+        return (Build.VERSION.SDK_INT >= VERSION_CODES_HONEYCOMB);
+    }
+
+    public static boolean isHoneycombMR1OrUp () {
+        return (Build.VERSION.SDK_INT >= VERSION_CODES_HONEYCOMB_MR1);
+    }
+
+    public static boolean isHoneycombMR2OrUp () {
+        return (Build.VERSION.SDK_INT >= VERSION_CODES_HONEYCOMB_MR2);
+    }
+
+    public static boolean isJellyBeanOrUp () {
+        return (Build.VERSION.SDK_INT >= VERSION_CODES_JELLY_BEAN);
+    }
+
+    public static boolean isJellyBeanMr1OrUp () {
+        return (Build.VERSION.SDK_INT >= VERSION_CODES_JELLY_BEAN_MR1);
+    }
+}
---
+++ src/main/java/library/utils/Screen.java
@@ -0,0 +1,93 @@
+package library.utils;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Display;
+
+public class Screen {
+
+    public enum ScreenSize {LARGE, NORMAL, SMALL, OTHER}
+    public enum ScreenDensity {LOW, MEDIUM, HIGH, XHIGH, OTHER}
+
+    public static int dpToPx(Resources resources, float dp) {
+        return (int) (TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.getDisplayMetrics()));
+    }
+
+    public static ScreenSize getScreenSize(Resources resources) {
+        //Determine screen size
+        if ((resources.getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE) {
+            return ScreenSize.LARGE;
+        } else if ((resources.getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_NORMAL) {
+            return ScreenSize.NORMAL;
+        } else if ((resources.getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_SMALL) {
+            return ScreenSize.SMALL;
+        } else {
+            return ScreenSize.OTHER;
+        }
+    }
+
+    public static ScreenDensity getScreenDesnsity(Resources resources) {
+        switch (resources.getDisplayMetrics().densityDpi) {
+            case DisplayMetrics.DENSITY_LOW:
+                return ScreenDensity.LOW;
+            case DisplayMetrics.DENSITY_MEDIUM:
+                return ScreenDensity.MEDIUM;
+            case DisplayMetrics.DENSITY_HIGH:
+                return ScreenDensity.HIGH;
+            case DisplayMetrics.DENSITY_XHIGH:
+                return ScreenDensity.XHIGH;
+            case DisplayMetrics.DENSITY_XXHIGH:
+                return ScreenDensity.XHIGH;
+            case DisplayMetrics.DENSITY_XXXHIGH:
+                return ScreenDensity.XHIGH;
+            default:
+                return ScreenDensity.OTHER;
+        }
+    }
+
+    public static boolean isLowOrMediumOrHighDensity(Resources resources) {
+        return (getScreenDesnsity(resources) == ScreenDensity.LOW ||
+                getScreenDesnsity(resources) == ScreenDensity.MEDIUM ||
+                getScreenDesnsity(resources) == ScreenDensity.HIGH ||
+                getScreenDesnsity(resources) == ScreenDensity.OTHER ||
+                getScreenDesnsity(resources) == ScreenDensity.XHIGH);
+    }
+
+    public static boolean isLowOrMediumDensity(Resources resources) {
+        return (getScreenDesnsity(resources) == ScreenDensity.LOW ||
+                getScreenDesnsity(resources) == ScreenDensity.MEDIUM ||
+                getScreenDesnsity(resources) == ScreenDensity.OTHER);
+    }
+
+    public static int getWidth(Activity a) {
+        Display display = a.getWindowManager().getDefaultDisplay();
+
+        if (AndroidVersion.isHoneycombMR2OrUp()) {
+            Point size = new Point();
+            display.getSize(size);
+            return size.x;
+        } else {
+            return display.getWidth();
+        }
+    }
+
+    public static int getHeight(Activity a) {
+        Display display = a.getWindowManager().getDefaultDisplay();
+
+        if (AndroidVersion.isHoneycombMR2OrUp()) {
+            Point size = new Point();
+            display.getSize(size);
+            return size.y;
+        } else {
+            return display.getHeight();
+        }
+    }
+}
+
