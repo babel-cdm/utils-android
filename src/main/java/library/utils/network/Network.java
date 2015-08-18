f2a4d5cc2d4976bf5bbc3d475e668e11b93327b3
Ismael González Sierra <ismael.gonzalez@babel.es>
2015-08-18 07:39:25 +0200

    Utilidades para el modulo Network

--- src/main/java/library/utils/network/Network.java
+++ src/main/java/library/utils/network/Network.java
@@ -3,6 +3,9 @@ package library.utils.network;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+import library.utils.AndroidVersion;
 
 @SuppressWarnings("unused")
 public class Network {
@@ -63,5 +66,195 @@ public class Network {
         boolean isMobileConn = networkInfo.isConnected();
 
         return (isWifiConn || isMobileConn);
+    }
+
+    /**
+     * Get the network info
+     *
+     * @param context
+     * @return
+     */
+    public static NetworkInfo getNetworkInfo(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo();
+    }
+
+    /**
+     * Check if there is any connectivity
+     *
+     * @param context
+     * @return
+     */
+    private static boolean isConnected(Context context) {
+        NetworkInfo info = getNetworkInfo(context);
+        return (info != null && info.isConnected());
+    }
+
+    /**
+     * Check if there is any connectivity to a Wifi network
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isConnectedWifi(Context context) {
+        NetworkInfo info = getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+    }
+
+    /**
+     * Check if there is any connectivity to a mobile network
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isConnectedMobile(Context context) {
+        NetworkInfo info = getNetworkInfo(context);
+        return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
+    }
+
+    /**
+     * Check if there is fast connectivity
+     * SDK Level Min 7
+     * @param context
+     * @return
+     */
+    public static boolean isConnectedFast(Context context) {
+        NetworkInfo info = getNetworkInfo(context);
+        return (info != null &&
+                (isConnectionFast(info.getType(), info.getSubtype()) ||
+                        isConnectionFastAPI8(info.getType(), info.getSubtype()) ||
+                        isConnectionFastAPI9(info.getType(), info.getSubtype()) ||
+                        isConnectionFastAPI11(info.getType(), info.getSubtype()) ||
+                        isConnectionFastAPI13(info.getType(), info.getSubtype()))
+        );
+    }
+
+    /**
+     * Check if the connection is fast
+     *
+     * @param type
+     * @param subType
+     * @return
+     */
+    private static boolean isConnectionFast(int type, int subType) {
+        if (type == ConnectivityManager.TYPE_WIFI) {
+            return true;
+        } else if (type == ConnectivityManager.TYPE_MOBILE) {
+            switch (subType) {
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                    return false; // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                    return false; // ~ 14-64 kbps
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                    return false; // ~ 50-100 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                    return true; // ~ 400-1000 kbps
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                    return true; // ~ 600-1400 kbps
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                    return false; // ~ 100 kbps
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                    return true; // ~ 2-14 Mbps
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                    return true; // ~ 700-1700 kbps
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                    return true; // ~ 1-23 Mbps
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                    return true; // ~ 400-7000 kbps
+                // Unknown
+                case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                default:
+                    return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean isConnectionFastAPI8(int type, int subType) {
+        if (AndroidVersion.isFroyoOrUp()) {
+            if (type == ConnectivityManager.TYPE_WIFI) {
+                return true;
+            } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                switch (subType) {
+                    case TelephonyManager.NETWORK_TYPE_IDEN: // API level 8
+                        return false; // ~25 kbps
+                    // Unknown
+                    case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                    default:
+                        return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean isConnectionFastAPI9(int type, int subType) {
+        if (AndroidVersion.isGingerbreadOrUp()) {
+            if (type == ConnectivityManager.TYPE_WIFI) {
+                return true;
+            } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                switch (subType) {
+                    case TelephonyManager.NETWORK_TYPE_EVDO_B: // API level 9
+                        return true; // ~ 5 Mbps
+                    // Unknown
+                    case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                    default:
+                        return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean isConnectionFastAPI11(int type, int subType) {
+        if (AndroidVersion.isHoneycombOrUp()) {
+            if (type == ConnectivityManager.TYPE_WIFI) {
+                return true;
+            } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                switch (subType) {
+
+                    case TelephonyManager.NETWORK_TYPE_EHRPD: // API level 11
+                        return true; // ~ 1-2 Mbps
+                    case TelephonyManager.NETWORK_TYPE_LTE: // API level 11
+                        return true; // ~ 10+ Mbps
+                    // Unknown
+                    case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                    default:
+                        return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean isConnectionFastAPI13(int type, int subType) {
+        if (AndroidVersion.isHoneycombMR2OrUp()) {
+            if (type == ConnectivityManager.TYPE_WIFI) {
+                return true;
+            } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                switch (subType) {
+                    case TelephonyManager.NETWORK_TYPE_HSPAP: // API level 13
+                        return true; // ~ 10-20 Mbps
+                    // Unknown
+                    case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                    default:
+                        return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
