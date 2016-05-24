43b917fcc148117bd5339ea29e5b26b8fdafc911
Javier Martínez Díaz <javier.martinez@babel-si.local>
2016-05-24 14:29:51 +0200

    Conversión del método haveNetworkConnection en estático

--- src/main/java/library/utils/network/Network.java
+++ src/main/java/library/utils/network/Network.java
@@ -12,7 +12,7 @@ public class Network {
 
     private static final String TAG_CLASS = Network.class.getSimpleName();
 
-    public boolean haveNetworkConnection(Context context) {
+    public static boolean haveNetworkConnection(Context context) {
         return (checkNetworkConnectionMode1(context) || checkNetworkConnectionMode2(context)
                 || checkNetworkConnectionMode3(context) || checkNetworkConnectionMode4(context));
     }
